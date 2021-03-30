442254e17926be639a13e21f03041a4e8c3207f0
Vagdevi Kommineni <30801221+vagdevik@users.noreply.github.com>
2021-03-30 17:48:43 +0700

    added code to be able to import pyspark on cxl lab
    added code to be able to import pyspark on cxl lab

--- spark/examples/dataframes/rdd_to_df.py
+++ spark/examples/dataframes/rdd_to_df.py
@@ -1,3 +1,16 @@
+# Uncomment the following in CloudxLab Lab if you can't import pyspark in Python3.
+
+# import os
+# import sys
+# os.environ["SPARK_HOME"] = "/usr/hdp/current/spark2-client"
+# os.environ["PYLIB"] = os.environ["SPARK_HOME"] + "/python/lib"
+# os.environ["PYSPARK_PYTHON"] = "/usr/local/anaconda/bin/python" 
+# os.environ["PYSPARK_DRIVER_PYTHON"] = "/usr/local/anaconda/bin/python"
+# sys.path.insert(0, os.environ["PYLIB"] +"/py4j-0.10.4-src.zip")
+# sys.path.insert(0, os.environ["PYLIB"] +"/pyspark.zip")
+
+# 
+
 from pyspark.sql.types import Row
 
 textRDD = sc.textFile("/data/spark/people.txt")
